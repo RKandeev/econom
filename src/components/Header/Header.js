5727d794826a0390bcd504503f7b8fefcd74e00d
Rustam <tatarin1605@gmail.com>
2023-03-11 15:22:58 +0300

    First commit

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import styles from './Header.module.scss'
 
-function Header(props) {
+function Header() {
     return (
-        <div className={styles.Header}>
-            <a href="/#"><img src="/img/logo.svg" alt="логотип"/></a>
-        </div>
+        <>
+            <div className={styles.Header}>
+                <a href="/#" className={styles.logo}><img src="/img/logo.svg" alt="логотип"/></a>
+                <div className={styles.links}>
+                    <a href="/#">О проекте</a>
+                    <a href="/#">Курсы</a>
+                    <a href="/#">Проверь себя</a>
+                    <a href="/#">Возможности</a>
+                </div>
+                <div className={styles.profileLinks}>
+                    <div className={styles.mybonus}>Мои баллы: 132</div>
+                    <a href="/#" className={styles.profileNav}><img src="/img/icon__notification.svg" alt="логотип"/></a>
+                    <a href="/#" className={styles.profileNav}><img src="/img/icon__user.svg" alt="логотип"/></a>
+                    <a href="/#" className={styles.profileNav}><img src="/img/icon__logout.svg" alt="логотип"/></a>
+                </div>
+            </div>
+        </>
     );
 }
-
 export default Header;
