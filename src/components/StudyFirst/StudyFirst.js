076a3982d551a3f7f52d5e7646fa055eca160292
Rustam <tatarin1605@gmail.com>
2024-03-11 14:44:16 +0300

    profile desktop

---
+++ src/components/StudyFirst/StudyFirst.js
@@ -0,0 +1,75 @@
+import React from "react";
+import styles from "./StudyFirst.module.scss";
+
+function StudyFirst(props) {
+  return (
+    <div className={styles.Studyfirst}>
+      <h2>Технология финансовой эффективности</h2>
+      <p>
+        Сайт рыбатекст поможет дизайнеру, верстальщику, вебмастеру сгенерировать
+        несколько абзацев более менее осмысленного текста рыбы на русском языке,
+        а начинающему оратору отточить навык публичных выступлений в домашних
+        условиях. При создании генератора мы использовали небезизвестный
+        универсальный код речей
+      </p>
+      <div className={styles.lesson_blocks}>
+        <div className={styles.lesson_block}>
+          <div className={styles.lesson_number}>Тема 1</div>
+          <div className={styles.lesson_title}>
+            Введение в управление финансами
+          </div>
+        </div>
+        <div className={styles.lesson_block}>
+          <div className={styles.lesson_number}>Тема 1</div>
+          <div className={styles.lesson_title}>
+            Введение в управление финансами
+          </div>
+        </div>
+        <div className={styles.lesson_block}>
+          <div className={styles.lesson_number}>Тема 1</div>
+          <div className={styles.lesson_title}>
+            Введение в управление финансами
+          </div>
+        </div>
+        <div className={styles.lesson_block}>
+          <div className={styles.lesson_number}>Тема 1</div>
+          <div className={styles.lesson_title}>
+            Введение в управление финансами
+          </div>
+        </div>
+        <div className={styles.lesson_block}>
+          <div className={styles.lesson_number}>Тема 1</div>
+          <div className={styles.lesson_title}>
+            Введение в управление финансами
+          </div>
+        </div>
+        <div className={styles.lesson_block}>
+          <div className={styles.lesson_number}>Тема 1</div>
+          <div className={styles.lesson_title}>
+            Введение в управление финансами
+          </div>
+        </div>
+        <div className={styles.lesson_block}>
+          <div className={styles.lesson_number}>Тема 1</div>
+          <div className={styles.lesson_title}>
+            Введение в управление финансами
+          </div>
+        </div>
+        <div className={styles.lesson_block}>
+          <div className={styles.lesson_number}>Тема 1</div>
+          <div className={styles.lesson_title}>
+            Введение в управление финансами
+          </div>
+        </div>
+        <div className={styles.lesson_block}>
+          <div className={styles.lesson_number}>Тема 1</div>
+          <div className={styles.lesson_title}>
+            Введение в управление финансами
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default StudyFirst;
---
+++ src/components/StudySecond/StudySecond.js
@@ -0,0 +1,84 @@
+import React from "react";
+import styles from "./StudySecond.module.scss";
+
+function StudySecond(props) {
+  return (
+    <div className={styles.StudyFirst}>
+      <h2>Технология выработки эффективных финансовых решений и стратегий</h2>
+      <p>
+        Сайт рыбатекст поможет дизайнеру, верстальщику, вебмастеру сгенерировать
+        несколько абзацев более менее осмысленного текста рыбы на русском языке,
+        а начинающему оратору отточить навык публичных выступлений в домашних
+        условиях.
+      </p>
+      <div className={styles.lesson_blocks}>
+        <div className={styles.lesson_block}>
+          <iframe
+            src="https://www.youtube.com/embed/LbOve_UZZ54?si=BS29VmHEDVik_zMT"
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+          ></iframe>
+        </div>
+        <div className={styles.lesson_block}>
+          <iframe
+            src="https://www.youtube.com/embed/LbOve_UZZ54?si=BS29VmHEDVik_zMT"
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+          ></iframe>
+        </div>
+        <div className={styles.lesson_block}>
+          <iframe
+            src="https://www.youtube.com/embed/LbOve_UZZ54?si=BS29VmHEDVik_zMT"
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+          ></iframe>
+        </div>
+        <div className={styles.lesson_block}>
+          <iframe
+            src="https://www.youtube.com/embed/LbOve_UZZ54?si=BS29VmHEDVik_zMT"
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+          ></iframe>
+        </div>
+        <div className={styles.lesson_block}>
+          <iframe
+            src="https://www.youtube.com/embed/LbOve_UZZ54?si=BS29VmHEDVik_zMT"
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+          ></iframe>
+        </div>
+        <div className={styles.lesson_block}>
+          <iframe
+            src="https://www.youtube.com/embed/LbOve_UZZ54?si=BS29VmHEDVik_zMT"
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+          ></iframe>
+        </div>
+        <div className={styles.lesson_block}>
+          <iframe
+            src="https://www.youtube.com/embed/LbOve_UZZ54?si=BS29VmHEDVik_zMT"
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+          ></iframe>
+        </div>
+        <div className={styles.lesson_block}>
+          <iframe
+            src="https://www.youtube.com/embed/LbOve_UZZ54?si=BS29VmHEDVik_zMT"
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+          ></iframe>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default StudySecond;
