d7da75a3524747e68b49771edf8547f5071dd2f5
Rustam <tatarin1605@gmail.com>
2024-12-04 19:57:50 +0300

    404 page

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -4,6 +4,7 @@ import MobileNav from '../../components/MobileNav/MobileNav';
 import styles from './NotFound.module.scss';
 import Lottie from 'react-lottie';
 import animationData from '../../img/json/404.json';
+import { Link } from 'react-router-dom';
 
 function NotFound(props) {
   const defaultOptions = {
@@ -16,8 +17,17 @@ function NotFound(props) {
     <>
       <Header />
       <MobileNav />
-      <div className={styles.anim404}>
-        <Lottie options={defaultOptions}></Lottie>
+      <div className={styles.notFoundPage}>
+        <div className={styles.anim404}>
+          <Lottie options={defaultOptions}></Lottie>
+        </div>
+        <div className={styles.text404}>
+          <h3>Ваша финансовая эффективность вышла за рамки реальности!</h3>
+          <p>
+            Вы можете пока перейти на <Link to='/'>Главную страницу</Link> для
+            оценки Ваших результатов{' '}
+          </p>
+        </div>
       </div>
     </>
   );
