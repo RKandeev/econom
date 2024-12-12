86f78c71560a7d4a3ab88c580f4f54ab77f69e9a
Rustam <tatarin1605@gmail.com>
2024-12-12 20:00:15 +0300

    404 page

--- src/App.js
+++ src/App.js
@@ -268,7 +268,7 @@ function App() {
             ></Route>
             <Route
               element={<AccountingBalance />}
-              path='lk/AccountingBalance'
+              path='AccountingBalance'
             ></Route>
             <Route path='*' element={<NotFound />}></Route>
           </Routes>
